c1e9f2e9cf87823cbb9822b4f12414f3112c792b
Alan Palazzolo <alan.palazzolo@startribune.com>
2019-01-09 15:04:07 -0600

    docs.

--- index.mjs
+++ index.mjs
@@ -1,5 +1,21 @@
+/**
+ * @ignore
+ * Main module entry point that provides the AirSupply class and
+ * factory function.
+ *
+ * @example
+ * const { AirSupply } = require('air-supply');
+ *
+ * @example
+ * // Assuming native NodeJS support
+ * import { AirSupply, airSupply } from 'air-supply/index';
+ *
+ * @module air-supply
+ */
+
 import AirSupply from './src/AirSupply';
 
+// Exports
 export default {
   AirSupply,
   // Create a wrapper function
